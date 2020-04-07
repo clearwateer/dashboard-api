fa556897fad2de6003ffa226c130d038c64f0c5f
Saad Ali Khan <saad.khan5891@gmail.com>
2020-04-07 23:01:18 +0500

    commit for post usermodel

--- src/main/java/com/dashboard/controllers/AuthController.java
+++ src/main/java/com/dashboard/controllers/AuthController.java
@@ -1,19 +1,17 @@
 package com.dashboard.controllers;
 
 import com.dashboard.models.UserModel;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * Created by Haseeb on 4/3/2020.
  */
 @RestController
 public class AuthController {
 
-    @RequestMapping({"/auth/token"})
+    @RequestMapping(value = "/auth/token", method = RequestMethod.POST)
     public String token(@RequestBody UserModel model) {
         System.out.println("Haseeb Khalid");
         return "Generate Auth Token";
     }
-
 }
