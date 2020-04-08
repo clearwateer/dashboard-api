38bcf7318633060d6bcb54306883a0ae428400d3
Saad Ali Khan <saad.khan5891@gmail.com>
2020-04-08 20:44:04 +0500

    added httpclient for auth token call

--- src/main/java/com/dashboard/controllers/AuthController.java
+++ src/main/java/com/dashboard/controllers/AuthController.java
@@ -2,6 +2,10 @@ package com.dashboard.controllers;
 
 import com.dashboard.models.UserModel;
 import org.springframework.web.bind.annotation.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  * Created by Haseeb on 4/3/2020.
@@ -12,6 +16,8 @@ public class AuthController {
     @RequestMapping(value = "/auth/token", method = RequestMethod.POST)
     public String token(@RequestBody UserModel model) {
         System.out.println("Haseeb Khalid");
+        String url = "http://localhost:5000/connect/token";
+
         return "Generate Auth Token";
     }
 }
---
+++ src/main/java/com/dashboard/services/AuthService.java
@@ -0,0 +1,65 @@
+package com.dashboard.services;
+
+import com.dashboard.models.UserModel;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+
+/**
+ * Created by saad on 4/7/2020.
+ */
+public class AuthService {
+
+    public String get_token(UserModel userModel){
+
+        try {
+
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(
+                    "http://localhost:5000/RESTfulExample/json/product/post");
+
+            StringEntity input = new StringEntity("{\"qty\":100,\"name\":\"iPad 4\"}");
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+
+            HttpResponse response = httpClient.execute(postRequest);
+
+            if (response.getStatusLine().getStatusCode() != 201) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            httpClient.getConnectionManager().shutdown();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+        return "Token";
+    }
+
+}
